dd616841ac0c779c6d730117db294ee640081e64
Nicole <nicolemibarra@icloud.com>
2019-07-07 15:24:54 -0700

    project 3

---
+++ build/README.md
@@ -0,0 +1,8 @@
+# Clicky Game
+
+#### Test your memory with the Alphabet game.
+
+#### Click on each letter once starting with A and ending with Z to win the game. But don't click on a letter twice, or it's game over for you!
+
+#### Built with React. 
+
---
+++ node_modules/react-fluid-container/lib/react-fluid-container.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = undefined;
+
+var _FluidContainer = require('./FluidContainer');
+
+var _FluidContainer2 = _interopRequireDefault(_FluidContainer);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _FluidContainer2.default;
+module.exports = exports['default'];
