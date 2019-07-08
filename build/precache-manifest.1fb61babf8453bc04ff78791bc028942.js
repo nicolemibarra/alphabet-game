4eaac3524b9b70bc006cdbdc7f583d0ed5f3c738
Nicole <nicolemibarra@icloud.com>
2019-07-07 21:00:49 -0700

    alphabet-game

---
+++ build/precache-manifest.1fb61babf8453bc04ff78791bc028942.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef6ea8d82cd277bced52c57af586744",
+    "url": "/alphabet-game/index.html"
+  },
+  {
+    "revision": "0144b9e05d59a6104ca5",
+    "url": "/alphabet-game/static/css/main.17539724.chunk.css"
+  },
+  {
+    "revision": "d9e3a54a145241b2b433",
+    "url": "/alphabet-game/static/js/2.4f9dc945.chunk.js"
+  },
+  {
+    "revision": "0144b9e05d59a6104ca5",
+    "url": "/alphabet-game/static/js/main.893352f7.chunk.js"
+  },
+  {
+    "revision": "95fa4251944441d6366b",
+    "url": "/alphabet-game/static/js/runtime~main.05da851d.js"
+  },
+  {
+    "revision": "5befb05dc8e18cdfabb15cea2c088f5d",
+    "url": "/alphabet-game/static/media/a.5befb05d.mp3"
+  },
+  {
+    "revision": "266c1f232e29fe57d16a54058e481adb",
+    "url": "/alphabet-game/static/media/b.266c1f23.mp3"
+  },
+  {
+    "revision": "a14bd2a0ba76610d070927715bbd4c65",
+    "url": "/alphabet-game/static/media/c.a14bd2a0.mp3"
+  },
+  {
+    "revision": "63ce6765ee0ede23402231b9d25cae7a",
+    "url": "/alphabet-game/static/media/d.63ce6765.mp3"
+  },
+  {
+    "revision": "20fb76ce19b9fda7eb9ef6404d584671",
+    "url": "/alphabet-game/static/media/e.20fb76ce.wav"
+  },
+  {
+    "revision": "88e309aee4cea37afb108689614800e7",
+    "url": "/alphabet-game/static/media/f.88e309ae.mp3"
+  },
+  {
+    "revision": "1e6115e85f53bf040d6b458d76f5bc1a",
+    "url": "/alphabet-game/static/media/g.1e6115e8.mp3"
+  },
+  {
+    "revision": "4111dc95d82c77addfd9d8d799e14f13",
+    "url": "/alphabet-game/static/media/h.4111dc95.mp3"
+  },
+  {
+    "revision": "1e47cdd57172a3055d130959c147f494",
+    "url": "/alphabet-game/static/media/i.1e47cdd5.mp3"
+  },
+  {
+    "revision": "160d540355e1fec99734053529d76f01",
+    "url": "/alphabet-game/static/media/j.160d5403.mp3"
+  },
+  {
+    "revision": "31fb795c17f0784294e78e616ea5695a",
+    "url": "/alphabet-game/static/media/k.31fb795c.mp3"
+  },
+  {
+    "revision": "7755c2c4f5ad734214b5ee7c472ff215",
+    "url": "/alphabet-game/static/media/l.7755c2c4.mp3"
+  },
+  {
+    "revision": "551bebcf2ab70ed9570a761999983a1c",
+    "url": "/alphabet-game/static/media/m.551bebcf.mp3"
+  },
+  {
+    "revision": "c61188392d5fe28d9a2358a9342066db",
+    "url": "/alphabet-game/static/media/n.c6118839.mp3"
+  },
+  {
+    "revision": "e9bde2dec2c4d576ffd9b9714a8a391d",
+    "url": "/alphabet-game/static/media/o.e9bde2de.mp3"
+  },
+  {
+    "revision": "3bb3a5d626dfc3be9a3e3ff1e39874ef",
+    "url": "/alphabet-game/static/media/p.3bb3a5d6.mp3"
+  },
+  {
+    "revision": "3c0b30ebb8b4c59d9b251b34cf02b3e0",
+    "url": "/alphabet-game/static/media/q.3c0b30eb.mp3"
+  },
+  {
+    "revision": "812115d2e8d20a0c6b686b24700c3cff",
+    "url": "/alphabet-game/static/media/r.812115d2.mp3"
+  },
+  {
+    "revision": "f003a79e6d93e7dd2d1badf2fbed6249",
+    "url": "/alphabet-game/static/media/s.f003a79e.mp3"
+  },
+  {
+    "revision": "97bc2416ff706d1d5addf2c174028364",
+    "url": "/alphabet-game/static/media/t.97bc2416.mp3"
+  },
+  {
+    "revision": "fca92dbcb1db7625a3e9ac69147ea54b",
+    "url": "/alphabet-game/static/media/u.fca92dbc.mp3"
+  },
+  {
+    "revision": "08ee15af31dba0c2d7708954f6fa4cf5",
+    "url": "/alphabet-game/static/media/v.08ee15af.mp3"
+  },
+  {
+    "revision": "ad6f6f3802979b739b7b9d13a9fe395f",
+    "url": "/alphabet-game/static/media/w.ad6f6f38.mp3"
+  },
+  {
+    "revision": "95768f1ae555fa08ed8bde454650aa98",
+    "url": "/alphabet-game/static/media/x.95768f1a.mp3"
+  },
+  {
+    "revision": "52b7bcf4fbe7f204de31eabb70690843",
+    "url": "/alphabet-game/static/media/y.52b7bcf4.mp3"
+  },
+  {
+    "revision": "f88d1338946476e9bb294a85ecda3a07",
+    "url": "/alphabet-game/static/media/z.f88d1338.mp3"
+  }
+]);
